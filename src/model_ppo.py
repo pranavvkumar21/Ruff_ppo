d20d700d0b2f3e8c6c969b61d549359eb39bad43
pranavvk21 <pranavvkumar21@gmail.com>
2022-08-08 17:17:23 +0100

    hopefully works

--- src/model_ppo.py
+++ src/model_ppo.py
@@ -8,10 +8,12 @@ import keras.backend as K
 gamma= 0.992
 lmbda = 0.95
 critic_discount = 0.5
-clip_range = 0.2
+clipping_val = 0.2
 entropy = 0.0025
 tfd = tfp.distributions
 
+act_optimizer = keras.optimizers.SGD(learning_rate=0.0003,clipnorm=1.0)
+cri_optimizer = keras.optimizers.SGD(learning_rate=0.0003,clipnorm = 1.0)
 
 def get_advantages(values, masks, rewards):
     returns = []
@@ -20,9 +22,32 @@ def get_advantages(values, masks, rewards):
         delta = rewards[i] + gamma * values[i + 1] * masks[i] - values[i]
         gae = delta + gamma * lmbda * masks[i] * gae
         returns.insert(0, gae + values[i])
+    #print(len(returns))
+    adv = np.array(returns).reshape((-1,1)) - np.array(values[:-1]).reshape((-1,1))
+    #print(np.mean(adv))
+    #print(np.std(adv))
+    adv = (adv - np.mean(adv)) / (np.std(adv) + 1e-10)
+    #print(adv.shape)
+    return returns, adv
 
-    adv = np.array(returns) - values[:-1]
-    return returns, (adv - np.mean(adv)) / (np.std(adv) + 1e-10)
+def ruff_train(actor,critic,rubuff,returns,advantages):
+    with tf.GradientTape(persistent = True) as tape:
+        old_log_probs = rubuff.logprobs
+        mu,sigma = actor(rubuff.states)
+
+        dist = tfd.Normal(loc=mu, scale=sigma)
+        new_log_probs = log_probs = dist.log_prob(rubuff.actions)
+        ratio = K.exp(new_log_probs - old_log_probs + 1e-10)
+        p1 = ratio * advantages
+        p2 = K.clip(ratio, min_value=1 - clipping_val, max_value=1 + clipping_val) * advantages
+        actor_loss = -K.mean(K.minimum(p1, p2))
+        critic_loss = K.mean(K.square(returns - critic(rubuff.states)))
+        print(actor_loss)
+        print(critic_loss)
+    actor_grads = tape.gradient(actor_loss, actor.trainable_variables)
+    critic_grads = tape.gradient(critic_loss, critic.trainable_variables)
+    act_optimizer.apply_gradients(zip(actor_grads, actor.trainable_variables))
+    cri_optimizer.apply_gradients(zip(critic_grads, critic.trainable_variables))
 
 def ppo_loss(oldpolicy_probs, advantages, rewards, values):
     def loss(y_true, y_pred):
@@ -48,24 +73,12 @@ def ppo_loss(oldpolicy_probs, advantages, rewards, values):
 
 def actor_Model(Input_shape,output_size):
     inputs = Input(shape=(Input_shape))
-    oldpolicy_probs = Input(shape=(1, output_size,))
-    advantages = Input(shape=(1, 1,))
-    rewards = Input(shape=(1, 1,))
-    values = Input(shape=(1, 1,))
-
     X = Dense(256, activation="relu", name="fc1")(inputs)
     X = Dense(256, activation="relu", name="fc2")(X)
     X = Dense(256, activation="relu", name="fc3")(X)
     mu = Dense(output_size, activation="tanh", name="mean")(X)
     sigma = Dense(output_size, activation="softplus", name="sigma")(X)
-    output = tf.keras.layers.Concatenate()([mu,sigma])
-
-    model = keras.Model(inputs=[inputs, oldpolicy_probs, advantages, rewards, values], outputs=output)
-    model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.00035,clipnorm=1.0), loss=[ppo_loss(
-        oldpolicy_probs=oldpolicy_probs,
-        advantages=advantages,
-        rewards=rewards,
-        values=values)])
+    model = keras.Model(inputs=inputs, outputs=[mu,sigma])
     try:
         model.load_weights("../model/ppo_actor.h5")
         print("loaded actor weights")
@@ -79,10 +92,7 @@ def critic_Model(Input_shape,output_size):
     X = Dense(256, activation="relu")(X)
     X = Dense(256, activation="relu")(X)
     X = Dense(output_size)(X)
-
     model = keras.Model(inputs=inputs, outputs=X)
-    model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.00035,clipnorm=1.0), loss='mse')
-
     try:
         model.load_weights("../model/ppo_critic.h5")
         print("loaded critic weights")
