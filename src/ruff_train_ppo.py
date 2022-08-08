aedb75488bf1c0c0057e4cffba92f146aa29ae44
pranavvk21 <pranavvkumar21@gmail.com>
2022-08-08 17:17:23 +0100

    hopefully works

--- src/ruff_train_ppo.py
+++ src/ruff_train_ppo.py
@@ -5,7 +5,7 @@ from ruff import *
 
 tfd = tfp.distributions
 
-NUM_EPISODES = 1_000
+NUM_EPISODES = 1
 STEPS_PER_EPISODE = 1_00
 timestep =1.0/240.0
 num_inputs = (60,)
@@ -22,7 +22,8 @@ bullet_file = "../model/test_ppo.bullet"
 filename = "ruff_logfile"
 reward_log = 'reward_logfile.csv'
 
-
+act_optimizer = keras.optimizers.SGD(learning_rate=0.0003,clipnorm=1.0)
+cri_optimizer = keras.optimizers.SGD(learning_rate=0.0003,clipnorm = 1.0)
 dummy_n = np.zeros((1, 1, 16))
 dummy_1 = np.zeros((1, 1, 1))
 
@@ -58,10 +59,8 @@ def check_log(filename):
 def run_episode(actor,critic,STEPS_PER_EPISODE,rubuff,ru,episode):
     for step in range(STEPS_PER_EPISODE):
         state_curr = ru.get_state()
-        output = actor([state_curr,dummy_n,dummy_1,dummy_1,dummy_1])
-        #print((output.shape))
-        (mu,sigma) = tf.split(output, num_or_size_splits=2, axis=1)
-        #print(mu.shape)
+        mu,sigma = actor([state_curr])
+
         critic_value = critic(state_curr)
         pos_inc, freq, actions, log_probs = ru.action_select(mu,sigma)
         ru.set_frequency(freq)
@@ -101,17 +100,14 @@ if __name__=="__main__":
         run_episode(actor,critic,STEPS_PER_EPISODE,rubuff,ru,episode)
         returns, advantages = get_advantages(rubuff.values, rubuff.masks, rubuff.rewards)
         rubuff.states = np.array(rubuff.states,dtype= "float32")
-        rubuff.logprobs = np.array(rubuff.logprobs,dtype= "float32")
+        rubuff.logprobs = np.array(rubuff.logprobs,dtype= "float32").reshape((-1,16))
         rubuff.states = np.reshape(rubuff.states, newshape=(-1, 60))
         rubuff.values = np.array(rubuff.values,dtype= "float32")
-        #rubuff.logprobs = np.zeros((100,1,1))
-        #print(np.reshape(rubuff.actions, newshape=(-1, n_actions)))
-        print(rubuff.states.shape)
-        actor_loss = actor.fit(
-        [rubuff.states, rubuff.logprobs, advantages, np.reshape(rubuff.rewards, newshape=(-1, 1, 1)), rubuff.values[:-1]],
-        [(np.reshape(rubuff.actions, newshape=(-1, n_actions))),(np.reshape(rubuff.actions, newshape=(-1, n_actions)))], verbose=True, shuffle=True, epochs=8)
-        critic_loss = critic.fit([states], [np.reshape(returns, newshape=(-1, 1))], shuffle=True, epochs=8,
-                                    verbose=True)
+        rubuff.actions = np.array(rubuff.actions,dtype= "float32").reshape((-1,16))
+        print("episode completed")
+        ruff_train(actor,critic,rubuff,returns,advantages)
+
+
         print(len(returns))
         print(len(advantages))
         save_model(actor,critic)
