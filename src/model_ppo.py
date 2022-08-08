fbb8814bb5a43c9a1c202abfe0906e4b290a8451
pranavvk21 <pranavvkumar21@gmail.com>
2022-08-08 17:23:55 +0100

    verbose added

--- src/model_ppo.py
+++ src/model_ppo.py
@@ -42,8 +42,6 @@ def ruff_train(actor,critic,rubuff,returns,advantages):
         p2 = K.clip(ratio, min_value=1 - clipping_val, max_value=1 + clipping_val) * advantages
         actor_loss = -K.mean(K.minimum(p1, p2))
         critic_loss = K.mean(K.square(returns - critic(rubuff.states)))
-        print(actor_loss)
-        print(critic_loss)
     actor_grads = tape.gradient(actor_loss, actor.trainable_variables)
     critic_grads = tape.gradient(critic_loss, critic.trainable_variables)
     act_optimizer.apply_gradients(zip(actor_grads, actor.trainable_variables))
