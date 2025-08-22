08b5e381e368070689067457a506cfef60616d01
pranavvkumar21 <pranavvkumar21@gmail.com>
2025-08-22 14:06:19 +0100

    run2 only forward velocity

--- src/ruff.py
+++ src/ruff.py
@@ -7,7 +7,6 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers import Input,Dense
 import tensorflow_probability as tfp
-from model_ppo import *
 import math
 from os.path import exists
 import os
@@ -43,53 +42,19 @@ urdf_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file_
 #print(os.listdir(os.path.dirname(os.path.abspath(__file__))))
 tfd = tfp.distributions
 
-urdf_constraint = [0,math.pi/6,math.pi/6]*4
-def setup_world(n_actors,client_mode):
-    physicsClient = p.connect(client_mode)##or p.DIRECT for no    n-graphical version
-    p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-    p.setGravity(0,0,-10)
-    planeId = p.loadURDF("plane.urdf")
-    ids = []
-    max_row = 3
-    c=0
-    d = 0
-    for i in range(n_actors):
-        print(i)
-        if d>=max_row:
-            c-=1
-        startPos = [0,d*3,0.4]
-        d+=1
-        startOrientation = p.getQuaternionFromEuler([0,0,math.pi/2])
-        boxId = p.loadURDF(urdf_path+"/ruff.urdf",startPos, startOrientation)
-        p.resetBasePositionAndOrientation(boxId, startPos,  startOrientation)
-        ids.append(boxId)
-    return ids
-
-
-def reset_world(filepath):
-    if exists(filepath):
-        p.restoreState(fileName=filepath)
-
-def save_world(filepath):
-    p.saveBullet(filepath)
-
-def close_world():
-    p.disconnect()
-
 
 class ruff:
     def __init__(self, id,command):
         self.id = id
-        #self.command = [1e-10,1.5,1e-10] #3 commands for motion
         self.command = command
-        #self.command[np.random.randint(2)+1] = np.random.rand()
-        #print(self.command)
         self.num_joints = p.getNumJoints(self.id)
         self.joint_names = {}
         for i in range(self.num_joints):
             self.joint_names[str(p.getJointInfo(self.id,i)[1])[2:-1]] = i
 
         self.n_joints = [i for i in range(self.num_joints) ]
+        self.joint_lower_limits = [p.getJointInfo(self.id,i)[2] for i in range(self.num_joints)]
+        self.joint_upper_limits = [p.getJointInfo(self.id,i)[3] for i in range(self.num_joints)]
         self.getjointinfo()    #12 joint position and 12 joint velocity
         self.getvelocity()     #6 base velocity velocity
         self.get_base_info()   # 3 base orientation
@@ -173,7 +138,8 @@ class ruff:
 
     def update_target_pos(self,pos_inc):
         for i in range(len(self.target_pos)):
-            self.target_pos[i]= self.joint_position[i] + pos_inc[i]
+            self.target_pos[i] += pos_inc[i]
+            self.target_pos[i] = np.clip(self.target_pos[i], self.joint_lower_limits[i], self.joint_upper_limits[i])
 
         return self.target_pos
 
@@ -204,26 +170,7 @@ class ruff:
     def update_policy(self,actions):
         self.prev_policy = self.policy.copy()
         self.policy = actions
-    def action_select(self,mu,sigma):
-        dist = tfd.Normal(loc=mu, scale=sigma)
-        actions = dist.sample(1)
-        actions = actions.numpy().tolist()[0][0]
-        pos_inc = actions[0:12]
-        for i in range(len(pos_inc)):
-            if pos_inc[i]<-1:
-                pos_inc[i]= (-1*math.pi/180)*6
-            elif pos_inc[i]>1:
-                pos_inc[i]= (1*math.pi/180)*6
-            else:
-                pos_inc[i]= (pos_inc[i]*math.pi/180)*6
 
-        #pos_inc = [i*math.pi/90 for i in pos_inc]
-        freq = np.abs(actions[12:])
-        for i in range(len(freq)):
-            if freq[i]>1:
-                freq[i]=1
-        log_probs = dist.log_prob(actions)
-        return pos_inc,freq, actions,log_probs
 
     def get_reward(self,kc=1):
         c1 = c2 = c3 = c4 =  1.2
@@ -316,12 +263,12 @@ class ruff:
                         kc["efficiency"]*(torque_penalty) + \
                         kc["efficiency"]*(velocity_penalty)
 
-        #self.reward = forward_velocity + kc*(lateral_velocity + angular_velocity)
+        self.reward = forward_velocity
         infos = {"rewards":rewards}
         return self.reward,infos
 
     def is_end(self):
-        if p.getContactPoints(self.id,0,linkIndexA=-1)!=(): #or p.getContactPoints(self.id,0,linkIndexA=0)!=() or p.getContactPoints(self.id,0,linkIndexA=1)!=() or p.getContactPoints(self.id,0,linkIndexA=3)!=() or p.getContactPoints(self.id,0,linkIndexA=4)!=() or p.getContactPoints(self.id,0,linkIndexA=6)!=() or p.getContactPoints(self.id,0,linkIndexA=7)!=() or p.getContactPoints(self.id,0,linkIndexA=9)!=() or p.getContactPoints(self.id,0,linkIndexA=10)!=() :
+        if p.getContactPoints(self.id,0,linkIndexA=-1)!=() or p.getContactPoints(self.id,0,linkIndexA=0)!=() or p.getContactPoints(self.id,0,linkIndexA=1)!=() or p.getContactPoints(self.id,0,linkIndexA=3)!=() or p.getContactPoints(self.id,0,linkIndexA=4)!=() or p.getContactPoints(self.id,0,linkIndexA=6)!=() or p.getContactPoints(self.id,0,linkIndexA=7)!=() or p.getContactPoints(self.id,0,linkIndexA=9)!=() or p.getContactPoints(self.id,0,linkIndexA=10)!=() :
             return 1
         else:
             return 0
